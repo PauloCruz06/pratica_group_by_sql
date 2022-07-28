644166936adee42ef3469a29974d342009ab7588
Paulo <pc5783346@gmail.com>
2022-07-28 14:27:40 -0300

    feat: questão 2

--- pratica_group_by.sql
+++ pratica_group_by.sql
@@ -1,3 +1,11 @@
 --Questão 1
 SELECT COUNT("endDate")
 FROM experiences;
+
+--Questão 2
+SELECT
+	"userId" AS id,
+	COUNT(status) AS educations
+FROM educations
+WHERE status = 'finished'
+GROUP BY "userId";
